350ddb617d97055635b657e4a4880df42bf255ad
Rodriguez <danielj.rodriguez@Weatherford.com>
2018-08-12 20:53:25 -0500

    datesubset.R function and plot1.R script ready
    Also added plot1.png generated by plot1.R script

--- datesubset.R
+++ datesubset.R
@@ -3,9 +3,34 @@
 #USAGE EXAMPLE: dfsubset <- datesubset(path = "./household_power_consumption.txt", date1 = "2007-02-01", date2 = "2007-02-03")
 
 datesubset <- function(path = "",date1 = "", date2 = ""){
+        #Load libraries
         library(dplyr)
+        
+        #Read data frame
         datesubset<-read.table(file = path, sep = ";", header = TRUE)
+        
+        #replace "?" missing values as NA.
         datesubset<-na_if(datesubset,"?")
+        
+        #Convert "Time" to a timestamp at GMT time zone.
+        datesubset$Time <- as.POSIXct(strptime(paste(as.character(foodatesubset$Date), as.character(foodatesubset$Time)),format = "%d/%m/%Y %H:%M:%OS",tz ="GMT"))
+        
+        #Convert "Date" from a "%d/%m/%Y" factor string format to a class Date column.
         datesubset$Date <- as.Date(datesubset$Date,"%d/%m/%Y")
+        
+        #COnvert "Time"from a "%d/%m/%Y" factor string format
+        
+        #Filter data frame to extract date interval of interest.
         datesubset <- filter(datesubset, Date >= date1 & Date < date2)
+        
+        #Data column conversions from factor, to character, to numeric
+        datesubset$Global_active_power<-as.numeric(as.character(datesubset$Global_active_power))
+        datesubset$Global_reactive_power<-as.numeric(as.character(datesubset$Global_reactive_power))
+        datesubset$Voltage<-as.numeric(as.character(datesubset$Voltage))
+        datesubset$Global_intensity<-as.numeric(as.character(datesubset$Global_intensity))
+        datesubset$Sub_metering_1<-as.numeric(as.character(datesubset$Sub_metering_1))
+        datesubset$Sub_metering_2<-as.numeric(as.character(datesubset$Sub_metering_2))
+        #NOTE:Sub_metering_3 is already of class numeric.
+        
+        datesubset
 }    
--- plot1.R
+++ plot1.R
@@ -1,11 +1,16 @@
 
 #Script to create plot1 for Global Active Power Histogram.
 
-
 #Read text file
+#NOTE:Edit path as needed for local system
 pathhpc <- "C:/Exploratory_Data_Analysis/ProjectWeek1/household_power_consumption.txt"
-hcdata <- read.table(file = path, sep = ";", header = TRUE)
 
 #Subset to select analysis days
 dfsubset <- datesubset(path = pathhpc, date1 = "2007-02-01", date2 = "2007-02-03")
 
+#Create Histogram for plot1
+hist(dfsubset$Global_active_power,col = "red", main = "Global Active Power", xlab = "Global Active Power (kilowatts)", ylab = "Frequency")
+
+#Create plot1.png file in current working directory
+dev.copy(png,'plot1.png')
+dev.off()
